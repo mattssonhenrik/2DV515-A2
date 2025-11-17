f06b9b546f28c2c0b13e9dfa01a65cb8344193ae
Henrik Mattsson <henkehenke@gmail.com>
2025-11-17 11:00:38 +0100

    Add basic structure for frontend and backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hm222yj-2dv515.a1"
+version = "0.0.1-SNAPSHOT"
+description = "backend for a1"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/src/main/java/hm222yj_2dv515/a1/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package hm222yj_2dv515.a1.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
